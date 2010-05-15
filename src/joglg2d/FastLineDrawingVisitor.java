d24f0a7342896f56daa6ed052c13744126bdf767
Brandon Borkholder <borkhobs@gmail.com>
2010-05-14 21:47:06 -0400

    Better implementation of FastLineDrawingVisitor that doesn't rely on buffers.

--- src/joglg2d/FastLineDrawingVisitor.java
+++ src/joglg2d/FastLineDrawingVisitor.java
@@ -1,7 +1,6 @@
 package joglg2d;
 
 import java.awt.BasicStroke;
-import java.nio.DoubleBuffer;
 
 import javax.media.opengl.GL;
 
@@ -15,9 +14,9 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   protected final BasicStroke stroke;
 
-  protected DoubleBuffer buffer = DoubleBuffer.allocate(500);
-
   protected double[] lastPoint;
+
+  protected double[] firstTwoPoints;
 
   public FastLineDrawingVisitor(GL gl, BasicStroke stroke) {
     this.gl = gl;
@@ -26,60 +25,71 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   @Override
   public void beginPoly(int windingRule) {
-    buffer.rewind();
   }
 
   @Override
   public void closeLine() {
-    double[] tmp = new double[4];
-    int pos = buffer.position();
-    buffer.rewind();
-    buffer.get(tmp);
-    buffer.position(pos);
-    buffer.put(tmp);
-    renderBuffer(GL.GL_QUAD_STRIP);
+    gl.glVertex2d(firstTwoPoints[0], firstTwoPoints[1]);
+    gl.glVertex2d(firstTwoPoints[2], firstTwoPoints[3]);
+    gl.glEnd();
   }
 
   @Override
   public void endPoly() {
-    renderBuffer(GL.GL_QUAD_STRIP);
+    gl.glEnd();
   }
 
   @Override
   public void lineTo(double[] vertex) {
     double offset = stroke.getLineWidth() / 2;
-    double[] thisPoint = new double[2];
+    double x, y;
     if (vertex[0] == lastPoint[0]) {
-      thisPoint[0] = lastPoint[0] - offset;
-      thisPoint[1] = lastPoint[1];
-      buffer.put(thisPoint);
-      thisPoint[0] = lastPoint[0] + offset;
-      thisPoint[1] = lastPoint[1];
-      buffer.put(thisPoint);
+      x = lastPoint[0] - offset;
+      y = lastPoint[1];
+      gl.glVertex2d(x, y);
+      x = lastPoint[0] + offset;
+      y = lastPoint[1];
+      gl.glVertex2d(x, y);
 
-      thisPoint[0] = lastPoint[0] - offset;
-      thisPoint[1] = vertex[1];
-      buffer.put(thisPoint);
-      thisPoint[0] = lastPoint[0] + offset;
-      thisPoint[1] = vertex[1];
-      buffer.put(thisPoint);
+      if (firstTwoPoints == null) {
+        firstTwoPoints = new double[4];
+        firstTwoPoints[0] = lastPoint[0] - offset;
+        firstTwoPoints[1] = lastPoint[1];
+        firstTwoPoints[2] = lastPoint[0] + offset;
+        firstTwoPoints[3] = lastPoint[1];
+      }
+
+      x = lastPoint[0] - offset;
+      y = vertex[1];
+      gl.glVertex2d(x, y);
+      x = lastPoint[0] + offset;
+      y = vertex[1];
+      gl.glVertex2d(x, y);
     } else {
       double angle = Math.atan((vertex[1] - lastPoint[1]) / (vertex[0] - lastPoint[0]));
       double sin = Math.sin(angle) * offset;
       double cos = Math.cos(angle) * offset;
-      thisPoint[0] = lastPoint[0] + sin;
-      thisPoint[1] = lastPoint[1] - cos;
-      buffer.put(thisPoint);
-      thisPoint[0] = lastPoint[0] - sin;
-      thisPoint[1] = lastPoint[1] + cos;
-      buffer.put(thisPoint);
+      x = lastPoint[0] + sin;
+      y = lastPoint[1] - cos;
+      gl.glVertex2d(x, y);
+      x = lastPoint[0] - sin;
+      y = lastPoint[1] + cos;
+      gl.glVertex2d(x, y);
 
-      thisPoint[0] = vertex[0] + sin;
-      thisPoint[1] = vertex[1] - cos;
-      buffer.put(thisPoint);
-      thisPoint[0] = vertex[0] - sin;
-      thisPoint[1] = vertex[1] + cos;
-      buffer.put(thisPoint);
+      if (firstTwoPoints == null) {
+        firstTwoPoints = new double[4];
+        firstTwoPoints[0] = lastPoint[0] + sin;
+        firstTwoPoints[1] = lastPoint[1] - cos;
+        firstTwoPoints[2] = lastPoint[0] - sin;
+        firstTwoPoints[3] = lastPoint[1] + cos;
+      }
+
+      x = vertex[0] + sin;
+      y = vertex[1] - cos;
+      gl.glVertex2d(x, y);
+      x = vertex[0] - sin;
+      y = vertex[1] + cos;
+      gl.glVertex2d(x, y);
     }
 
     lastPoint = vertex;
@@ -87,24 +97,9 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   @Override
   public void moveTo(double[] vertex) {
-    lastPoint = vertex;
-    renderBuffer(GL.GL_QUAD_STRIP);
-  }
-
-  protected void renderBuffer(int mode) {
-    if (buffer.position() == 0) {
-      return;
-    }
-
-    buffer.limit(buffer.position());
-    buffer.rewind();
-    gl.glBegin(mode);
-    while (buffer.hasRemaining()) {
-      gl.glVertex2d(buffer.get(), buffer.get());
-    }
     gl.glEnd();
-
-    buffer.rewind();
-    buffer.limit(buffer.capacity());
+    gl.glBegin(GL.GL_QUAD_STRIP);
+    lastPoint = vertex;
+    firstTwoPoints = null;
   }
 }
