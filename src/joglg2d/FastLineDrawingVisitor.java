26dcc1ab860741cbd327d99061296eb4d868955f
Brandon Borkholder <borkhobs@gmail.com>
2010-05-16 11:41:39 -0400

    Slightly better method to outline with BasicStroke, but it's nowhere near complete.

--- src/joglg2d/FastLineDrawingVisitor.java
+++ src/joglg2d/FastLineDrawingVisitor.java
@@ -16,7 +16,7 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   protected double[] lastPoint;
 
-  protected double[] firstTwoPoints;
+  protected double[] firstPoint;
 
   public FastLineDrawingVisitor(GL gl, BasicStroke stroke) {
     this.gl = gl;
@@ -29,8 +29,7 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   @Override
   public void closeLine() {
-    gl.glVertex2d(firstTwoPoints[0], firstTwoPoints[1]);
-    gl.glVertex2d(firstTwoPoints[2], firstTwoPoints[3]);
+    lineTo(firstPoint);
     gl.glEnd();
   }
 
@@ -51,14 +50,6 @@ public class FastLineDrawingVisitor implements VertexVisitor {
       y = lastPoint[1];
       gl.glVertex2d(x, y);
 
-      if (firstTwoPoints == null) {
-        firstTwoPoints = new double[4];
-        firstTwoPoints[0] = lastPoint[0] - offset;
-        firstTwoPoints[1] = lastPoint[1];
-        firstTwoPoints[2] = lastPoint[0] + offset;
-        firstTwoPoints[3] = lastPoint[1];
-      }
-
       x = lastPoint[0] - offset;
       y = vertex[1];
       gl.glVertex2d(x, y);
@@ -76,14 +67,6 @@ public class FastLineDrawingVisitor implements VertexVisitor {
       y = lastPoint[1] + cos;
       gl.glVertex2d(x, y);
 
-      if (firstTwoPoints == null) {
-        firstTwoPoints = new double[4];
-        firstTwoPoints[0] = lastPoint[0] + sin;
-        firstTwoPoints[1] = lastPoint[1] - cos;
-        firstTwoPoints[2] = lastPoint[0] - sin;
-        firstTwoPoints[3] = lastPoint[1] + cos;
-      }
-
       x = vertex[0] + sin;
       y = vertex[1] - cos;
       gl.glVertex2d(x, y);
@@ -100,6 +83,6 @@ public class FastLineDrawingVisitor implements VertexVisitor {
     gl.glEnd();
     gl.glBegin(GL.GL_QUAD_STRIP);
     lastPoint = vertex;
-    firstTwoPoints = null;
+    firstPoint = vertex;
   }
 }
