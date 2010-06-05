afcfb533d84bf341e57e336686585bf9ac2c5436
Brandon Borkholder <borkhobs@gmail.com>
2010-06-05 08:49:38 -0400

    Just cleaned up the code a bit.

--- src/joglg2d/FastLineDrawingVisitor.java
+++ src/joglg2d/FastLineDrawingVisitor.java
@@ -17,7 +17,6 @@
 package joglg2d;
 
 import java.awt.BasicStroke;
-import java.util.Arrays;
 
 import javax.media.opengl.GL;
 
@@ -77,7 +76,26 @@ public class FastLineDrawingVisitor implements VertexVisitor {
   }
 
   @Override
+  public void moveTo(double[] vertex) {
+    if (firstPoint != null) {
+      drawLineEnd();
+    }
+
+    gl.glEnd();
+
+    if (firstPoint != null) {
+      applyEndCap(lastPoint, secondLastPoint);
+      applyEndCap(firstPoint, secondPoint);
+    }
+
+    gl.glBegin(GL.GL_QUAD_STRIP);
+    firstPoint = lastPoint = vertex;
+    secondLastPoint = secondPoint = null;
+  }
+
+  @Override
   public void lineTo(double[] vertex) {
+    // ignore 0-length lines
     if (lastPoint[0] == vertex[0] && lastPoint[1] == vertex[1]) {
       return;
     }
@@ -98,8 +116,8 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   protected void drawLineEnd() {
     double angle = angleOf(lastPoint, secondLastPoint);
-    double cos = Math.cos(angle) * offset;
-    double sin = Math.sin(angle) * offset;
+    double cos = cos(angle) * offset;
+    double sin = sin(angle) * offset;
 
     gl.glVertex2d(lastPoint[0] + sin, lastPoint[1] - cos);
     gl.glVertex2d(lastPoint[0] - sin, lastPoint[1] + cos);
@@ -107,8 +125,8 @@ public class FastLineDrawingVisitor implements VertexVisitor {
 
   protected void drawLineStart() {
     double angle = angleOf(secondPoint, firstPoint);
-    double cos = Math.cos(angle) * offset;
-    double sin = Math.sin(angle) * offset;
+    double cos = cos(angle) * offset;
+    double sin = sin(angle) * offset;
 
     gl.glVertex2d(firstPoint[0] + sin, firstPoint[1] - cos);
     gl.glVertex2d(firstPoint[0] - sin, firstPoint[1] + cos);
@@ -118,117 +136,82 @@ public class FastLineDrawingVisitor implements VertexVisitor {
     double angle1 = angleOf(lastPoint, secondLastPoint);
     double angle2 = angleOf(vertex, lastPoint);
 
-    double cos1 = Math.cos(angle1) * offset;
-    double sin1 = Math.sin(angle1) * offset;
-    double cos2 = Math.cos(angle2) * offset;
-    double sin2 = Math.sin(angle2) * offset;
-
-    if (Arrays.equals(lastPoint, secondLastPoint)) {
-      gl.glVertex2d(lastPoint[0] + sin2, lastPoint[1] - cos2);
-      gl.glVertex2d(lastPoint[0] - sin2, lastPoint[1] + cos2);
+    switch (stroke.getLineJoin()) {
+    case BasicStroke.JOIN_BEVEL:
+      drawBevelCorner(angle1, angle2, vertex);
       return;
-    } else if (Arrays.equals(lastPoint, vertex)) {
-      gl.glVertex2d(lastPoint[0] + sin1, lastPoint[1] - cos1);
-      gl.glVertex2d(lastPoint[0] - sin1, lastPoint[1] + cos1);
+
+    case BasicStroke.JOIN_ROUND:
+      drawRoundCorner(angle1, angle2, vertex);
+      return;
+
+    case BasicStroke.JOIN_MITER:
+      drawMiterCorner(angle1, angle2, vertex);
+      return;
+
+    default:
+      throw new IllegalStateException("This class cannot support unknown line join");
+    }
+  }
+
+  protected void drawRoundCorner(double angle1, double angle2, double[] vertex) {
+    double sin1 = sin(angle1) * offset;
+    double sin2 = sin(angle2) * offset;
+    double cos1 = cos(angle1) * offset;
+    double cos2 = cos(angle2) * offset;
+
+    gl.glVertex2d(lastPoint[0] + sin1, lastPoint[1] - cos1);
+    gl.glVertex2d(lastPoint[0] - sin1, lastPoint[1] + cos1);
+    if (angle2 < angle1) {
+      angle2 += Math.PI * 2;
+    }
+
+    for (double angle = angle1; angle < angle2; angle += 0.5) {
+      double sin = sin(angle);
+      double cos = cos(angle);
+      gl.glVertex2d(lastPoint[0] + sin * offset, lastPoint[1] - cos * offset);
+      gl.glVertex2d(lastPoint[0] - sin * offset, lastPoint[1] + cos * offset);
+    }
+    gl.glVertex2d(lastPoint[0] + sin2, lastPoint[1] - cos2);
+    gl.glVertex2d(lastPoint[0] - sin2, lastPoint[1] + cos2);
+  }
+
+  protected void drawBevelCorner(double angle1, double angle2, double[] vertex) {
+    double sin1 = sin(angle1) * offset;
+    double sin2 = sin(angle2) * offset;
+    double cos1 = cos(angle1) * offset;
+    double cos2 = cos(angle2) * offset;
+
+    gl.glVertex2d(lastPoint[0] + sin1, lastPoint[1] - cos1);
+    gl.glVertex2d(lastPoint[0] - sin1, lastPoint[1] + cos1);
+    gl.glVertex2d(lastPoint[0] + sin2, lastPoint[1] - cos2);
+    gl.glVertex2d(lastPoint[0] - sin2, lastPoint[1] + cos2);
+  }
+
+  protected void drawMiterCorner(double angle1, double angle2, double[] vertex) {
+    if (exceedsMiterLimit(angle1, angle2)) {
+      drawBevelCorner(angle1, angle2, vertex);
       return;
     }
 
-    if (stroke.getLineJoin() == BasicStroke.JOIN_BEVEL) {
-      gl.glVertex2d(lastPoint[0] + sin1, lastPoint[1] - cos1);
-      gl.glVertex2d(lastPoint[0] - sin1, lastPoint[1] + cos1);
-      gl.glVertex2d(lastPoint[0] + sin2, lastPoint[1] - cos2);
-      gl.glVertex2d(lastPoint[0] - sin2, lastPoint[1] + cos2);
-      return;
-    } else if (stroke.getLineJoin() == BasicStroke.JOIN_ROUND) {
-      gl.glVertex2d(lastPoint[0] + sin1, lastPoint[1] - cos1);
-      gl.glVertex2d(lastPoint[0] - sin1, lastPoint[1] + cos1);
-      if (angle2 < angle1) {
-        angle2 += Math.PI * 2;
-      }
-
-      for (double angle = angle1; angle < angle2; angle += 0.5) {
-        double sin = Math.sin(angle);
-        double cos = Math.cos(angle);
-        gl.glVertex2d(lastPoint[0] + sin * offset, lastPoint[1] - cos * offset);
-        gl.glVertex2d(lastPoint[0] - sin * offset, lastPoint[1] + cos * offset);
-      }
-      gl.glVertex2d(lastPoint[0] + sin2, lastPoint[1] - cos2);
-      gl.glVertex2d(lastPoint[0] - sin2, lastPoint[1] + cos2);
-      return;
-    }
-    // else BasicStroke.JOIN_MITER and continue
+    double sin1 = sin(angle1) * offset;
+    double sin2 = sin(angle2) * offset;
+    double cos1 = cos(angle1) * offset;
+    double cos2 = cos(angle2) * offset;
 
     double[] pt1 = new double[] { secondLastPoint[0] + sin1, secondLastPoint[1] - cos1 };
     double[] pt2 = new double[] { lastPoint[0] + sin2, lastPoint[1] - cos2 };
     double[] pt3 = new double[] { lastPoint[0] - secondLastPoint[0], lastPoint[1] - secondLastPoint[1] };
     double[] pt4 = new double[] { vertex[0] - lastPoint[0], vertex[1] - lastPoint[1] };
     double[] intersect = intersection(pt1, pt2, pt3, pt4);
-
-    if (magnitude(pt3) > 0 && magnitude(pt4) > 0) {
-      gl.glVertex2d(intersect[0], intersect[1]);
-    } else {
-      System.out.println(Arrays.toString(intersect));
-    }
+    gl.glVertex2d(intersect[0], intersect[1]);
 
     pt1 = new double[] { secondLastPoint[0] - sin1, secondLastPoint[1] + cos1 };
     pt2 = new double[] { lastPoint[0] - sin2, lastPoint[1] + cos2 };
     pt3 = new double[] { lastPoint[0] - secondLastPoint[0], lastPoint[1] - secondLastPoint[1] };
     pt4 = new double[] { vertex[0] - lastPoint[0], vertex[1] - lastPoint[1] };
     intersect = intersection(pt1, pt2, pt3, pt4);
-    if (magnitude(pt3) > 0 && magnitude(pt4) > 0) {
-      gl.glVertex2d(intersect[0], intersect[1]);
-    }
-  }
-
-  static double[] intersection(double[] origin1, double[] origin2, double[] vect1, double[] vect2) {
-    if (Math.abs(vect1[0] / vect2[0] - vect1[1] / vect2[1]) < 1e-10 && magnitude(vect1) > 0 && magnitude(vect2) > 0) {
-      System.out.println();
-    }
-
-    double[] crossV = crossProduct(vect1, vect2);
-    double[] diffP = new double[] { origin2[0] - origin1[0], origin2[1] - origin1[1] };
-    double[] crossPV = crossProduct(diffP, vect2);
-
-    if (Math.abs(crossV[0] / crossPV[0] - crossV[1] / crossPV[1]) < 1e-10 && magnitude(crossV) > 0 && magnitude(crossPV) > 0) {
-      System.out.println();
-    }
-
-    double a = magnitude(crossPV) / magnitude(crossV);
-
-    double[] pt = new double[] { origin1[0] + a * vect1[0], origin1[1] + a * vect1[1] };
-    return pt;
-  }
-
-  static double magnitude(double[] v) {
-    double s = 0;
-    for (double i : v) {
-      s += i * i;
-    }
-
-    return Math.sqrt(s);
-  }
-
-  static double[] crossProduct(double[] a, double[] b) {
-    // XXX assuming a, b are in the x,y plane
-    return new double[] { 0, 0, a[0] * b[1] - a[1] * b[0] };
-  }
-
-  @Override
-  public void moveTo(double[] vertex) {
-    if (firstPoint != null) {
-      drawLineEnd();
-    }
-
-    gl.glEnd();
-
-    if (firstPoint != null) {
-      applyEndCap(lastPoint, secondLastPoint);
-      applyEndCap(firstPoint, secondPoint);
-    }
-
-    gl.glBegin(GL.GL_QUAD_STRIP);
-    firstPoint = lastPoint = vertex;
-    secondLastPoint = secondPoint = null;
+    gl.glVertex2d(intersect[0], intersect[1]);
   }
 
   protected void applyEndCap(double[] vertex, double[] other) {
@@ -241,8 +224,8 @@ public class FastLineDrawingVisitor implements VertexVisitor {
     case BasicStroke.CAP_SQUARE:
       gl.glBegin(GL.GL_QUADS);
       double angle = angleOf(vertex, other);
-      double sin = Math.sin(angle) * offset;
-      double cos = Math.cos(angle) * offset;
+      double sin = sin(angle) * offset;
+      double cos = cos(angle) * offset;
       double x = vertex[0] + sin;
       double y = vertex[1] - cos;
       gl.glVertex2d(x, y);
@@ -262,17 +245,85 @@ public class FastLineDrawingVisitor implements VertexVisitor {
     case BasicStroke.CAP_ROUND:
       gl.glBegin(GL.GL_TRIANGLE_FAN);
       angle = angleOf(vertex, other);
-      gl.glVertex2d(vertex[0] + Math.cos(angle) * .1, vertex[1] + Math.sin(angle) * .1);
+      gl.glVertex2d(vertex[0] + cos(angle) * .1, vertex[1] + sin(angle) * .1);
       double step = 0.5;
       double maxAngle = angle + Math.PI + step;
       for (double theta = angle; theta < maxAngle; theta += step) {
-        x = vertex[0] + Math.sin(theta) * offset;
-        y = vertex[1] - Math.cos(theta) * offset;
+        x = vertex[0] + sin(theta) * offset;
+        y = vertex[1] - cos(theta) * offset;
         gl.glVertex2d(x, y);
       }
       gl.glEnd();
       break;
     }
+  }
+
+  /**
+   * Returns true if the miter join is not appropriate for these angles. This
+   * not only takes into account the miter limit of BasicStroke, but also
+   * determines if <code>angle1</code> and <code>angle2</code> are such that the
+   * derivation of the miter points would encounter an error.
+   */
+  protected boolean exceedsMiterLimit(double angle1, double angle2) {
+    // TODO
+    return false;
+  }
+
+  /**
+   * Finds the intersection of two lines in parametric form. Letting
+   * <code>o1=origin1</code> and <code>v1=vect1</code> (similarly for
+   * <code>origin2, vect2</code>) we have that
+   *
+   * <pre>
+   * P1 = o1 + t * v1
+   * P2 = o2 + s * v2
+   * </pre>
+   *
+   * We can solve to find the intersection by
+   *
+   * <pre>
+   * o1 + t * v1 = o2 + s * v2
+   * t * v1 = o2 - o1 + s * v2
+   * (t * v1) x v2 = (o2 - o1 + s * v2) x v2    ; cross product by v2
+   * t * (v1 x v2) = (o2 - o1) x v2             ; to get rid of s term
+   * </pre>
+   *
+   * Solve for <code>t</code> by finding the ratio of the magnitudes. Now just
+   * put <code>t</code> back into the first equation gives us our point of
+   * intersection. Note that I didn't come up with this, but it's pretty easy to
+   * determine.
+   */
+  static double[] intersection(double[] origin1, double[] origin2, double[] vect1, double[] vect2) {
+    // v1 x v2
+    double crossVz = crossProductZ(vect1, vect2);
+    // o2 - o1
+    double[] diffP = new double[] { origin2[0] - origin1[0], origin2[1] - origin1[1] };
+    // (o2 - o1) x v2
+    double crossPVz = crossProductZ(diffP, vect2);
+
+    double t = crossPVz / crossVz;
+    double[] pt = new double[] { origin1[0] + t * vect1[0], origin1[1] + t * vect1[1] };
+    return pt;
+  }
+
+  /**
+   * Since all our drawing is in the x,y plane, the only non-zero component of
+   * the cross-product is z.
+   */
+  protected static double crossProductZ(double[] a, double[] b) {
+    return a[0] * b[1] - a[1] * b[0];
+  }
+
+  /*
+   * XXX I don't know how slow cos is, but given how much I use it, this might
+   * be optimized to the first three terms of the Taylor series.
+   */
+  protected static double cos(double angle) {
+    return Math.cos(angle);
+  }
+
+  protected static double sin(double angle) {
+    return Math.sin(angle);
   }
 
   protected static double angleOf(double[] vertex, double[] other) {
