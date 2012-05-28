4aac681cd87007a488230b2415c3ce6de4380297
Brandon Borkholder <borkhobs@gmail.com>
2012-05-28 10:19:12 -0400

    Using the maven-license-plugin to check and insert the licenses.

--- src/main/java/glg2d/impl/shader/GL2ES2ShapeDrawer.java
+++ src/main/java/glg2d/impl/shader/GL2ES2ShapeDrawer.java
@@ -1,19 +1,18 @@
-/**************************************************************************
-   Copyright 2012 Brandon Borkholder
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- ***************************************************************************/
-
+/*
+ * Copyright 2012 Brandon Borkholder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package glg2d.impl.shader;
 
 import glg2d.GLGraphics2D;
@@ -42,7 +41,6 @@ public class GL2ES2ShapeDrawer extends AbstractShapeHelper {
     simpleFillVisitor.setGLContext(g2d.getGLContext().getGL());
   }
 
-  @Override
   public void draw(Shape shape) {
     Stroke stroke = getStroke();
     if (stroke instanceof BasicStroke) {
--- src/main/java/glg2d/impl/shader/GL2ES2TransformHelper.java
+++ src/main/java/glg2d/impl/shader/GL2ES2TransformHelper.java
@@ -1,19 +1,18 @@
-/**************************************************************************
-   Copyright 2012 Brandon Borkholder
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- ***************************************************************************/
-
+/*
+ * Copyright 2012 Brandon Borkholder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package glg2d.impl.shader;
 
 import glg2d.GLG2DTransformHelper;
@@ -49,7 +48,6 @@ public class GL2ES2TransformHelper implements GLG2DTransformHelper {
     GL gl = g2d.getGLContext().getGL();
     gl.glGetIntegerv(GL.GL_VIEWPORT, viewportDimensions, 0);
   }
-
   @Override
   public void push(GLGraphics2D newG2d) {
     transformStack.push((AffineTransform) getTransform0().clone());
