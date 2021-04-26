e8dfe6f8d9c6e7a177983a25ac24fa7f5b58a40e
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2021-04-26 19:54:54 +0200

    Round corners issue repro

--- examples-jogl/src/main/java/net/opengrabeso/glg2d/examples/G2DExample.java
+++ examples-jogl/src/main/java/net/opengrabeso/glg2d/examples/G2DExample.java
@@ -28,63 +28,65 @@ public class G2DExample extends JFrame {
         G2DExample main = new G2DExample();
         main.display();
     }
+
+
+    static class CustomComponent extends JComponent {
+
+        private static final long serialVersionUID = 1L;
+
+        private final int margin = 10;
+
+        @Override
+        public Dimension getMinimumSize() {
+            return new Dimension(100, 100);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(400, 300);
+        }
+
+        private Rectangle placeRect(Rectangle rect) {
+            Dimension dim = getSize();
+
+            int wMax = dim.width - margin - rect.x;
+            int hMax = dim.height - margin - rect.y;
+
+            return new Rectangle(
+                    rect.x,
+                    rect.y,
+                    Math.min(rect.width, wMax),
+                    Math.min(rect.height, hMax)
+            );
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            int margin = 10;
+            Dimension dim = getSize();
+            super.paintComponent(g);
+            g.setColor(Color.red);
+            g.fillRect(margin, margin, dim.width - margin * 2, dim.height - margin * 2);
+
+            {
+                Rectangle rect = placeRect(new Rectangle(20, 20, 300, 300));
+                g.setColor(Color.yellow);
+                g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 100, 100);
+            }
+
+            {
+                Rectangle rect = placeRect(new Rectangle(100, 100, 100, 100));
+                g.setColor(Color.green);
+                g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 20, 20);
+            }
+
+            {
+                Rectangle rect = placeRect(new Rectangle(300, 100, 200, 200));
+                g.setColor(Color.blue);
+                g.fillOval(rect.x, rect.y, rect.width, rect.height);
+
+            }
+        }
+    }
 }
 
-class CustomComponent extends JComponent {
-
-    private static final long serialVersionUID = 1L;
-
-    private final int margin = 10;
-
-    @Override
-    public Dimension getMinimumSize() {
-        return new Dimension(100, 100);
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(400, 300);
-    }
-
-    private Rectangle placeRect(Rectangle rect) {
-        Dimension dim = getSize();
-
-        int wMax = dim.width - margin - rect.x;
-        int hMax = dim.height - margin - rect.y;
-
-        return new Rectangle(
-                rect.x,
-                rect.y,
-                Math.min(rect.width, wMax),
-                Math.min(rect.height, hMax)
-        );
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        int margin = 10;
-        Dimension dim = getSize();
-        super.paintComponent(g);
-        g.setColor(Color.red);
-        g.fillRect(margin, margin, dim.width - margin * 2, dim.height - margin * 2);
-
-        {
-            Rectangle rect = placeRect(new Rectangle(20, 20, 300, 300));
-            g.setColor(Color.yellow);
-            g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 100, 100);
-        }
-
-        {
-            Rectangle rect = placeRect(new Rectangle(100, 100, 100, 100));
-            g.setColor(Color.green);
-            g.fillRoundRect(rect.x, rect.y, rect.width, rect.height, 20, 20);
-        }
-
-        {
-            Rectangle rect = placeRect(new Rectangle(300, 100, 200, 200));
-            g.setColor(Color.blue);
-            g.fillOval(rect.x, rect.y, rect.width, rect.height);
-
-        }
-    }
-}
