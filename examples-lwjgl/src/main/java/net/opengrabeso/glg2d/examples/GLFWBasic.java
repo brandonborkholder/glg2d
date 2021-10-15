09240159afb4b116fc2fca34eae496de37b3c0a8
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2021-10-15 09:54:38 +0200

    Started LWJGL implementation

---
+++ examples-lwjgl/src/main/java/net/opengrabeso/glg2d/examples/GLFWBasic.java
@@ -0,0 +1,10 @@
+package net.opengrabeso.glg2d.examples;
+
+import net.opengrabeso.glg2d.GLG2DPanelLWJGL;
+
+public class GLFWBasic {
+    public static void main(String[] args) {
+        new GLG2DPanelLWJGL().run();
+    }
+
+}
