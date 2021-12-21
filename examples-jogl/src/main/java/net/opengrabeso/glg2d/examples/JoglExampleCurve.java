9c77cd1a451b523625fa83b2b24839b2def6d8fc
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2021-12-21 09:47:08 +0100

    Demonstrate shape issue

---
+++ examples-jogl/src/main/java/net/opengrabeso/glg2d/examples/JoglExampleCurve.java
@@ -0,0 +1,7 @@
+package net.opengrabeso.glg2d.examples;
+
+public class JoglExampleCurve {
+    public static void main(String[] args) {
+        JoglExampleFactory.display(new G2DExampleCurve());
+    }
+}
---
+++ examples-lwjgl/src/main/java/net/opengrabeso/glg2d/examples/LWJGLG2DExampleCurve.java
@@ -0,0 +1,7 @@
+package net.opengrabeso.glg2d.examples;
+
+public class LWJGLG2DExampleCurve {
+    public static void main(String[] args) {
+        LWJGLExampleFactory.display(new G2DExampleCurve());
+    }
+}
